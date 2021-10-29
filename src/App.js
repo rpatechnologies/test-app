6ec1339313297249d873dacf639f30bfbdabc762
Ravi <raviatpwt@gmail.com>
2021-10-29 18:32:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect } from "react";
 function App() {
+  const rooms = [
+    { room_type: "Queen", vacant_rooms: 5, price: 100 },
+    { room_type: "Double", vacant_rooms: 3, price: 75 },
+    { room_type: "Twin", vacant_rooms: 8, price: 60 },
+  ];
+  const formatPrice = (price) => {
+    return "$" + price;
+  };
+
+  //basic case is coverd the number which is divisible
+  //by 14 can also be divisible by 2 and 7, those cases are not covered due
+  //to lake of clarification about the functionlity required
+  const checkNum = (num) => {
+    if (num % 14 == 0) {
+      console.log("foobar");
+    } else if (num % 7 == 0) {
+      console.log("bar");
+    } else if (num % 2 == 0) {
+      console.log("foo");
+    } else {
+      console.log(num);
+    }
+  };
+
+  useEffect(() => {
+    checkNum(14);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ol>
+        {rooms.map((item, index) => {
+          return (
+            <li>
+              {item.room_type},{item.vacant_rooms}, {formatPrice(item.price)}
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 }
